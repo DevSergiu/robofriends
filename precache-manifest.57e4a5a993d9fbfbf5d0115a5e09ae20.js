d49bae7f94b6eb503c9f684d36acf1b0dc3a2c3b
Sergiu-19 <42917431+Sergiu-19@users.noreply.github.com>
2019-06-16 23:15:19 +0300

    Updates

---
+++ precache-manifest.57e4a5a993d9fbfbf5d0115a5e09ae20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c894a7cf908257eff0ad2cad94107c00",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "3c6cdba5cde8b2cc5ac9",
+    "url": "/robofriends/static/css/main.29f2e798.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/robofriends/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "3c6cdba5cde8b2cc5ac9",
+    "url": "/robofriends/static/js/main.4e8d13dc.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
